1313840b7b60a68364f70151d2a014a39758ec96
Abdulloh Mahmudjanov <mahmudjanovabdulloh358@gmail.com>
2025-03-09 00:20:17 +0300

    Ikkinchi bora yuklanish

--- src/Pages/Tags/Tags.jsx
+++ src/Pages/Tags/Tags.jsx
@@ -1,5 +1,6 @@
 import "./Tags.css";
 import Doctype from "./Doctype/Doctype";
+import { TAGSDATA } from "../../Mock";
 
 const Tags = () => {
   return (
@@ -7,6 +8,13 @@ const Tags = () => {
       <div className="container">
         <section className="tags">
           <Doctype />
+          {TAGSDATA.map((item, index) => {
+            return (
+              <div className="tagsdata-div" key={index}>
+                <h1>{item.TAGSDATAHeading}</h1>
+              </div>
+            );
+          })}
         </section>
       </div>
     </main>
